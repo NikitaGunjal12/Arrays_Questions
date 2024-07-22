ac731bd7bc542f3f86b20a4d52725bcca771dd45
NikitaGunjal12 <nikitagunjal.rmdstic.comp@gmail.com>
2024-07-22 22:51:49 +0530

    arrayQue

---
+++ ArraysQue.java
@@ -0,0 +1,106 @@
+package Arrays_Questions;
+
+public class ArraysQue {
+/*Linear Search
+Linear search is used to search a key element from multiple elements.
+Linear search is less used today because it is slower than binary search
+and hashing.
+*/
+
+/*public class LinearSearchEx{
+    public static int LinearSearchex(int arr[],int key){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==key){
+                return i;
+            }
+        }
+        return key;
+     }
+    public static void main(String args[]){
+        int arr[]={3,5,6,7,8,9,10};
+        int key =10;
+        int index = LinearSearchex(arr, key);
+        if(index==-1){
+            System.out.println("key not found");
+        }else{
+            System.out.println("key is found at index:"+index);
+        }
+    }
+}*/
+
+/*
+
+ //Example 2: LARGEST AND SMALLEST ELEMENT FROM ARRAY...
+public class LargestNoFromArray {
+    public static int LargestEle(int arr[]){
+        int max= Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(max<arr[i]){
+                max=arr[i];
+            }
+            if(min>arr[i]){
+                min = arr[i];
+            }
+        }
+     System.out.println("The Smallest Number is:"+min);
+    return max;
+    }
+    public static void main(String args[]){
+        int arr[]={1,3,5,6,89};
+        System.out.println("The Largest Element is:"+LargestEle(arr));
+    }
+}
+ */
+
+ /*
+Example :3
+
+BINARY SEARCH
+Binary search is used to search a key element from multiple elements.
+ Binary search is faster than linear search.
+In case of binary search, array elements must be in ascending order. 
+If you have unsorted array,you can sort the array using Arrays.sort(arr) method
+
+
+import java.util.Arrays;
+public class BinarySearch {
+    public static int BinarySearchEx(int arr[],int key){
+        int start=0;
+        int end =arr.length-1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(arr[mid]==key){
+                return mid;
+
+            }if (arr[mid]<key){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[]args){
+        int arr[]={34,56,12,89,32};
+        int key =12;
+        System.out.println("The Element at Index:"+BinarySearchEx(arr, key));
+
+    }
+
+//EXAMPLE 2:(Another method of BinarySearch)
+public static void main(String[]args){
+    int arr[]={10,20,30,40,50,60};
+    int key =40;
+    int result = Arrays.binarySearch(arr,key);
+    if(result <0){
+        System.out.println("Element is not found");
+    }else{
+        System.out.println("Element found at index:"+result);
+    }
+}
+}
+ */
+
+    
+}
