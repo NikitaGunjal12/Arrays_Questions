984aa3642bd3dc1f816e5e7892c926263e80c08e
NikitaGunjal12 <nikitagunjal.rmdstic.comp@gmail.com>
2024-07-25 16:05:12 +0530

    newprogram

--- ArraysQue.java
+++ ArraysQue.java
@@ -459,7 +459,63 @@ public class TrappedRainWater {
          * return nums;
          */
 
- }
+/*
+ Example:11
+public class DivideConquer {
+    /*public static void printarr(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+ "");
+        }
+        System.out.println();
+    }
+    public static void mergeSort(int arr[], int si, int ei){
+        if(si>=ei){
+            return;
+        }
+        int mid = si+(ei-si)/2;
+        mergeSort(arr, si, mid);
+        mergeSort(arr, mid+1, ei);
+        merge(arr, si, mid, ei);
+
+    }
+    public static void merge(int arr[], int si,int ei,int mid){
+        //left(0,3)=4   right(4,6)=3  -->6-0+1=7
+        int temp[]=new int [ei-si+1];
+        int i=si;  //left part
+        int j= mid+1;   //right part
+        int k =0;   // iterator for temp arr
+        while(i<= mid &&  j<=ei){
+            if(arr[i]< arr[j]){
+                temp[k]=arr[i];
+                i++; 
+            }else{
+                temp[k]=arr[j];
+                j++;
+            }
+            k++;
+        }
+        //Left part
+        while(i<=mid){
+            temp[k++] =arr[i++];
+        }
+        //Right part
+        while(j<=ei){
+            temp[k++]= arr[j++];
+        }
+     for(k=0, i=si; k<temp.length;k++,i++){
+            arr[i] = temp[k];
+        }
+    }
+    public static void main(String[]args){
+        int arr[]={6,3,9,5,2,8};
+        mergeSort(arr, 0, arr.length-1);
+        printarr(arr);
+    }*/
+
+    
+}
+
+ 
     
   
   
