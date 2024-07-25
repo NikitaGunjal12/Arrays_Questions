37a431431e0077b7b25c555da7ded816dbb569aa
NikitaGunjal12 <nikitagunjal.rmdstic.comp@gmail.com>
2024-07-25 16:26:00 +0530

    new program

--- ArraysQue.java
+++ ArraysQue.java
@@ -170,11 +170,33 @@ public class Program3 {
         }
         
     }
-    
-    
-
-
  */
+
+ /*EXAMPLE:6
+ public class PairsOfArrays {
+    public static void PrintPairs(int nums[]){
+        int tp=0;
+        for(int i=0;i<nums.length;i++){
+            int curr = nums[i];
+            for(int j=i+1; j<nums.length;j++){
+                System.out.print("(" +curr +"," + nums[j]+ ")");
+                tp++;
+
+            }
+            System.out.println();
+
+        }
+        System.out.println("Total Numbers Of Pairs:"+tp);
+    }
+    public static void main(String[]args){
+        int nums[] ={2,4,6,8,10};
+        PrintPairs(nums);
+
+    }
+}
+
+
+  */
 
   
 
