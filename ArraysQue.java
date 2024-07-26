e4995a052a1777fc71a88a016ba9f3d2ee199f85
NikitaGunjal12 <nikitagunjal.rmdstic.comp@gmail.com>
2024-07-26 18:53:53 +0530

    commit1

--- ArraysQue.java
+++ ArraysQue.java
@@ -220,6 +220,29 @@ public class Program3 {
 }
 
  */
+/*
+ Example:8
+ import java.util.Scanner;
+public class ArmstrongNo{
+	public static void main(String args[]){
+		int n,arm=0,rem,c;
+		System.out.println("Enter the number:");
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		c=n;
+		while(n>0){
+			rem=n%10;
+			arm=(rem*rem*rem)+arm;
+			n=n/10;
+		}
+		if(c==arm)
+			System.out.println("Armstrong number");
+		else
+			System.out.println("no is not armstrong");
+	}
+}
+
+ */
 
   
 
