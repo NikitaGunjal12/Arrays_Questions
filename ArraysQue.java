b97232b6a349712bd21368785aacf37a5f80b773
NikitaGunjal12 <nikitagunjal.rmdstic.comp@gmail.com>
2024-07-26 18:56:56 +0530

    commit1

--- ArraysQue.java
+++ ArraysQue.java
@@ -244,6 +244,27 @@ public class ArmstrongNo{
 }
 
  */
+/*
+ Example :9
+ import java.util.Scanner;
+public class LeapYear{
+	public static void main(String[]args){
+		int year;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the year");
+		year = sc.nextInt();
+		if(year%400==0){
+			System.out.println("Year is leap");
+		}
+		else{
+			System.out.println("Year is Not Leap");
+		}
+	}
+}
+
+			
+ */
 
   
 
